c192b1e01866f856f7fd703f356fdf6c86ffb4ec
OnlyByDesign <benyannella@gmail.com>
2020-02-03 12:40:31 -0500

    routine update | reorged JS

--- js/main.js
+++ js/main.js
@@ -1,28 +1,41 @@
-const $ = jQuery;
+//const $ = jQuery;
+//const page = ()=> document.title.split(' ', 1).toString().toLowerCase();
+const DOM = {
+	home: document.querySelector(".page-id-598"),
+	about: document.querySelector(".page-id-594")
+};
 const parallaxScroll = (parallax) => {
 	Object.values(parallax).forEach(function(current) {
-		window.addEventListener( "scroll", ()=> document.getElementsByClassName(current[0])[0].style.transform = "translateY(" + (window.pageYOffset * current[1] + "px") + ')');
+		window.addEventListener( "scroll", ()=> document.getElementsByClassName(current[0])[0].style.transform = "translateY(" + (window.pageYOffset * current[1] + "px") + ')' );
 	});
 };
-
 const parallaxItems = {
 	home: {
 		0: [ "hero__main", 0.1 ],
 		1: [ "hero__sub--slider", -0.15 ],
 		2: [ "scroll-bottom", -0.05 ],
 		3: [ "intro__main--subtitle", -0.1 ],
-		//4: [ "intro__copy--sub", 0.05 ],
-		5: [ "work__left", -0.025 ],
-		6: [ "work__right", 0.025 ]	
+		4: [ "work__left", -0.025 ],
+		5: [ "work__right", 0.025 ]	
+	},
+	about: {
+		0: [ "about__image", -0.15 ],
+		1: [ "about__copy", 0.1 ]
 	}
+};
+
+function onScroll() {
+	// window.addEventListener( "scroll", ()=> parallaxScroll(parallaxItems.page) );
 };
 function home() {
 	parallaxScroll(parallaxItems.home);
 	document.querySelector(".intro__button").setAttribute("download", "BY-Resume");
 };
-function onScroll() {
-	//window.addEventListener( "scroll", func );	
+function about() {
+	parallaxScroll(parallaxItems.about);
 };
+
 (()=> {
-	if (document.getElementsByClassName(".page-id-598") !== null) home();
+	if (document.body.contains(DOM.home)) home();
+	if (document.body.contains(DOM.about)) home();
 })();
