ace05c17371c6491bede564a673ffd15a28c367d
OnlyByDesign <benyannella@gmail.com>
2020-02-03 12:40:49 -0500

    added SCSS support

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
+define('WP_SCSS_ALWAYS_RECOMPILE', true);
+
 /*function load_scripts_custom() {
     //wp_enqueue_script('jquery', get_stylesheet_directory_uri() . '/js/jquery.latest.js' );
     //wp_enqueue_script('gsap', get_stylesheet_directory_uri() . '/js/gsap.min.js' );
